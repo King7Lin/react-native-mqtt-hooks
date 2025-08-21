cc735631b2b8137e28606eac0a4ca30fcd517a26
King7Lin <1978331141@qq.com>
2025-08-21 09:22:30 +0800

    test

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ export const useMqttService = (config = {}) => {
     return dangerousFunction(5);
   };
   unsafeFunction()
+  console.log('123123');
+  
   // 连接MQTT服务器
   const connect = useCallback((onStatusChange) => {
     //onStatusChange:(status,error)=>{}
